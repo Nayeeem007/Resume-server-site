4cd4f45f35ac9a9e7c648b5da07602a558d33553
Md. Abu Nayeem <niloy1882@gmail.com>
2023-05-21 18:13:35 +0600

    middleware added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
+
 
 app.get('/',(req,res)=>{
     res.send('toy server is running')
