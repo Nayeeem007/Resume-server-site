3ba389c63b78718a69346886bf5698554ffdf3ad
Md. Abu Nayeem <niloy1882@gmail.com>
2023-05-21 18:14:42 +0600

    middleware problem solved

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(cors())
+
 
 app.get('/',(req,res)=>{
     res.send('toy server is running')
